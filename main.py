e85354032cba0ffbb3c3d455b3b5f8fb95fa4bfe
kylomassey <128275043+kylomassey@users.noreply.github.com>
2026-03-09 04:50:34 +0000

    added filter

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def main():
     print("Music Playlist Tool")
     song1 = Song("Here we are", "The Journey", "Still here", 2009, 110, "rock", 1.5, 2, 30)
     song2 = Song("Sacred Sword", "The Journey", "Still here", 2009, 110, "rock", 1.5, 4, 20)
-    song3 = Song("Flight to the stars", "The Journey", "Still here", 2009, 110, "rock", 1.5, 5, 30)
+    song3 = Song("Flight to the stars", "My Mystery", "Vagabond", 2009, 110, "rock", 1.5, 5, 30)
     mysongs = Playlist()
     mysongs.add_song(song1)
     mysongs.add_song(song2)
     mysongs.add_song(song3)
     print(mysongs.duration())
-    mysongs.format_duration()
     mysongs.print_songs()
+    mysongs.filter(genre = "rock", album = "Vag")
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -42,12 +42,23 @@ class Playlist:
         self.totaltime = time
         return format_duration(time)
     
-    def print_songs(self):
+    def print_songs(self, list = None):
         cnt = 1
+        if not list:
+            list = self.songs
         print("\nYour Playlist")
-        for song in self.songs:
+        for song in list:
             print(f"{cnt}) {song.name}")
             cnt += 1
 
-    def filter(self, arg, param):
-        return 0
+    def filter(self, genre = None, artist = None, album = None):
+        results = []
+        for song in self.songs:
+            if genre is not None and genre.lower() != song.genre.lower():
+                continue
+            if artist is not None and artist.lower() not in song.artist.lower():
+                continue
+            if album is not None and album.lower() not in song.album.lower():
+                continue
+            results.append(song)
+        self.print_songs(results)
